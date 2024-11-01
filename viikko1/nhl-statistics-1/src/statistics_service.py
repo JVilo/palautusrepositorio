03b7e8480aa264c4c17ef98f268eafb429148e1f
JVilo <vilo.johanna@gmail.com>
2024-11-01 13:22:17 +0200

    Tasks for tasks for week 1 done, moved next week's task elsewhere

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,12 +1,16 @@
 from player_reader import PlayerReader
+from enum import Enum
 
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self,player_reader:PlayerReader):
 
-        self._players = reader.get_players()
+        self._players = player_reader.get_players()
 
     def search(self, name):
         for player in self._players:
@@ -23,21 +27,33 @@ class StatisticsService:
 
         return list(players_of_team)
 
-    def top(self, how_many):
+    def top(self, how_many,sort_by=None):
+        sort_by = sort_by
         # metodin käyttämä apufufunktio voidaan määritellä näin
         def sort_by_points(player):
-            return player.points
+            if sort_by == None:
+                return player.points
+            if sort_by.value == 1:
+                return player.points
+            if sort_by.value == 2:
+                return player.goals
+            if sort_by.value == 3:
+                return player.assists
 
         sorted_players = sorted(
             self._players,
             reverse=True,
             key=sort_by_points
         )
+        #print(str(sorted_players))
 
         result = []
         i = 0
-        while i <= how_many:
+        while i <= how_many-1:
             result.append(sorted_players[i])
             i += 1
+                
 
         return result
+    
+    
